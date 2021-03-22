44954c212c284521c39cf8e057343ad8cbd9a5b2
AJ <ancheetah@users.noreply.github.com>
2021-03-22 14:42:32 -0400

    display items

--- src/components/ToDoItemsComponent.js
+++ src/components/ToDoItemsComponent.js
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 class ToDoItems extends Component {
+    // createTask(item) {
+    //     return <li key={item.key}>{item.text}</li>
+    // }
 
-}
+    render() {
+        return (
+            <ListGroup>
+                <ListGroupItem>
+                    {this.props.entries.map( item => {
+                        return <li key={item.key}>{item.text}</li>
+                    })}
+                </ListGroupItem>
+            </ListGroup>
+        );
+    }
+};
 
 export default ToDoItems;
